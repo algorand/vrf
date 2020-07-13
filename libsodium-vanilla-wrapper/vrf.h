b3dde76a7802a74b84d11631622b3f5f5fdad047
Adam <adam@algorand.com>
2020-07-13 15:15:53 -0400

    Update README

--- libsodium-vanilla-wrapper/vrf.h
+++ libsodium-vanilla-wrapper/vrf.h
@@ -1,3 +1,4 @@
+// See vrf.c for documentation
 int vrf_prove(unsigned char proof[80], const unsigned char skpk[64], const unsigned char *msg, unsigned long long msglen);
 int vrf_verify(unsigned char output[64], const unsigned char pk[32], const unsigned char proof[80], const unsigned char *msg, unsigned long long msglen);
 
